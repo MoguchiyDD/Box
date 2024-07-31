f7d5d063532533a5985950b68b94aa516e08c858
MoguchiyDD <ket-ket-01@bk.ru>
2024-07-31 17:46:30 +0500

    feat: Test No.3

---
+++ excel/zero/tests/2/test.py
@@ -0,0 +1,35 @@
+# МогучийДД (MoguchiyDD)
+# 2024.07.28, 08:42:46 PM
+# test.py
+
+
+from sys import path as syspath
+from os import path as ospath
+
+syspath.append(
+    ospath.abspath(
+        ospath.join(
+            ospath.dirname(__file__),
+            "../../../"
+        )
+    )
+)
+
+from zero.convert_from_excel_to_json import ConvertFromExcelToJSON
+
+
+# ------------ VARIABLES ------------
+
+EXCEL = "../excelfile.test.xlsx"
+SCHEMA = "schema.test.json"
+
+# -----------------------------------
+
+
+if __name__ == "__main__":
+    convert = ConvertFromExcelToJSON(EXCEL, SCHEMA, "result.test")
+    convert.cmd(convert.Commands.open)
+    convert.cmd(convert.Commands.schema)
+    convert.cmd(convert.Commands.sheet)
+    convert.cmd(convert.Commands.getDataAndLoad)
+    convert.cmd(convert.Commands.close)

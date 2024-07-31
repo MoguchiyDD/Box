0f8e6b0d70825c3b90b824bfd74affd17ca3274a
MoguchiyDD <ket-ket-01@bk.ru>
2024-07-31 17:19:13 +0500

    feat: «open» command

---
+++ excel/zero/convert_from_excel_to_json.py
@@ -0,0 +1,55 @@
+# МогучийДД (MoguchiyDD)
+# 2024.07.28, 12:06:35 PM
+# convert_from_excel_to_json.py
+
+
+from enum import Enum
+
+from openpyxl import Workbook, load_workbook
+
+
+class ConvertFromExcelToJSON:
+    """
+    Convert from EXCEL to JSON
+
+    PARAMETERS:
+    - **filepath** : an EXCEL file
+
+    FUNCTIONS:
+    - **cmd**(type: Commands) -> Workbook : Running a class via available
+    commands
+    """
+
+    class Commands(Enum):
+        open = "open"
+
+    def __init__(self, filepath: str) -> None:
+        self.filepath: str = filepath
+        self.workbook: None | Workbook = None
+
+    def cmd(self, type: Commands) -> Workbook:
+        """
+        Running a class via available commands
+
+        PARAMETERS:
+        - **type** : Variable from the **Command** class
+
+        COMMANDS:
+        - «**open**» : Opening an EXCEL file
+
+        RETURN: the commands «**open**» have data return, and the rest nothing
+        """
+
+        if type is self.Commands.open:
+            self.__enter__()
+            return self.workbook
+        
+        return None
+
+    def __enter__(self) -> None:
+        """
+        Opening an EXCEL file \\
+        **Launch via the «open» command**
+        """
+
+        self.workbook: Workbook = load_workbook(self.filepath, data_only=True)
